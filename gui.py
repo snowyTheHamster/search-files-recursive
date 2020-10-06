47fbe4a22a2f6dad4bb07d093bfb7649c09146a2
Taku <vidafuku@gmail.com>
2020-10-06 12:11:23 +0800

    added file extention filter

--- gui.py
+++ gui.py
@@ -4,32 +4,30 @@ import time
 import shutil
 
 
-sg.theme('Dark Blue 3') # Add Color
+sg.theme('Dark Blue 17') # Add Color
+
 
 # All the stuff inside you window
 layout = [
-    [sg.Text('Find all files scattered in a folder and')],
-    [sg.Text('duplicate them into one folder ')],
     [sg.Text('')],
-    [sg.Text('HOW TO USE:')],
-    [sg.Text('1. Select input folder')],
-    [sg.Text('2. Select output folder')],
-    [sg.Text('3. Select files extensions (optional)')],
-    [sg.Text('4. Select file size (optional)')],
-    [sg.Text('5. Click View Results')],
-    [sg.Text('6. Click Copy Results')],
-    [sg.Text('Select Input Folder'), sg.Input(key='_DIRINPUT_'), sg.FolderBrowse(),],
-    [sg.Text('(Don\'t leave blank)')],
-    [sg.Text('- - - - - - - - - - - - - - - - -')],
+    [sg.Text('FILTER FILES BY EXTENSION:')],
+    [sg.Text('e.g: .jpg'), sg.InputText(key='_CUSTOM_EXT_')],
+    [sg.Text('( Leave blank to disable filter )')],
+    [sg.Text('')],
+    [sg.Text('SEARCH FILES IN THIS FOLDER RECURSIVELY:')],
+    [sg.Text('Search folder'), sg.Input(key='_DIRINPUT_'), sg.FolderBrowse(),],
+    [sg.Button("Search")],
+    [sg.Text('')],
+    [sg.Text('')],
+    [sg.Text('COPY FILES TO THIS FOLDER:')],
     [sg.Text('Select Output Folder'), sg.Input(key='_DIROUTPUT_'), sg.FolderBrowse(),],
-    [sg.Text('- - - - - - - - - - - - - - - - -')],
-    [sg.Text('Filter filetype eg: jpg'), sg.InputText(key='_FILEEXT_')],
-    [sg.Text('(leave blank to disable filter)')],
-    [sg.OK("Generate"), sg.Cancel(), sg.Cancel("Quit")]
+    [sg.Text('( If you don\'t select Output Folder, nothing will happen. )')],
+    [sg.Button("Copy Files"), sg.Cancel("Exit")],
+    [sg.Text('')]
 ]
 
 # Create the Window
-window = sg.Window('SEARCH & COPY FILES', layout)
+window = sg.Window('RECURSIVE SEARCH & COPY FILES', layout)
 
 # Event Loop to process "events" and get the "values" of the inputs
 while True:
@@ -39,36 +37,68 @@ while True:
     if event in (None, 'Cancel', 'Quit'):
         break
 
-    #start time
-    stime = time.time()
+    # get start time
+    starttime = time.time()
 
     dir_input = values['_DIRINPUT_']
     dir_output = values['_DIROUTPUT_']
-    file_ext = values['_FILEEXT_']
-
-    for dirpath, dirnames, filenames in os.walk(dir_input):
-        for f in filenames:
-            ext = os.path.splitext(f)[-1] # gets the file ext
-            files = (os.path.join(dirpath, f)) # full path of file
-            size = os.stat(files).st_size # filesize info in bytes
-            sizeinmb = round(size/1000000, 2) #filesize rounded off to 2 decimal places
-
-            file_input_fullpath = (os.path.join(dirpath, f)) # full path of input file
-            file_output_fullpath = (os.path.join(dir_output, f)) # full path of output file
-
-            # print(f'filename: {f}, filetype: {ext}, filesize: {sizeinmb}MB') # display file info
-            # print(f'copying {files} to {file_output_fullpath} filesize: {sizeinmb}MB') # print a generic message            
-            # shutil.copy2(files, file_output_fullpath) # copy/pasto the files to output folder
-            print(file_ext)
-
-
-    #end time
-    etime = time.time()
+    custom_ext = values['_CUSTOM_EXT_']
     
-    totaltime = etime - stime
-    totaltime = round(totaltime, 1)
 
-    print(f'Done processing in: {totaltime} seconds \n')
-    print('You may now close the program...')
+    # show alert if search folder empty
+    if not dir_input:    
+        sg.popup('Search Folder can\'t be empty')
+
+    else:
+        file_info = []
+        for dirpath, dirnames, filenames in os.walk(dir_input):
+            for f in filenames:
+                ext = os.path.splitext(f)[-1] # file extension
+                files = (os.path.join(dirpath, f)) # file fullpath
+                size = os.stat(files).st_size # filesize (bytes)
+                sizeinmb = round(size/1000000, 2) #filesize (MB)
+                file_input_fullpath = (os.path.join(dirpath, f))
+                file_output_fullpath = (os.path.join(dir_output, f))
+
+
+                # copy files btn (clicked)
+                if event == 'Copy Files':
+                    if dir_output: # output_folder selected
+
+                        # file_extension filter
+                        if custom_ext: 
+                            if ext == custom_ext:
+                                print(f'copying {files} to {file_output_fullpath} filesize: {sizeinmb}MB')
+                                shutil.copy2(files, file_output_fullpath)
+                        
+                        # no filter
+                        else:
+                            print(f'copying {files} to {file_output_fullpath} filesize: {sizeinmb}MB')
+                            shutil.copy2(files, file_output_fullpath)
+
+
+                # search btn (clicked)
+                if event == 'Search':
+
+                    # file_extension filter
+                    if custom_ext:
+                        if ext == custom_ext:
+                            file_info.append(f'{f} ({sizeinmb}MB)')
+
+                    # no filter
+                    else:
+                        file_info.append(f'{f} ({sizeinmb}MB)')
+        
+
+        # Displays file info when search is clicked
+        if event == 'Search':
+            sg.popup(file_info)
+
+
+        # time taken to run script
+        endtime = time.time()
+        totaltime = endtime - starttime
+        totaltime = round(totaltime, 1)
+        print(f'Done processing in: {totaltime} seconds. \n')
 
 window.close()
